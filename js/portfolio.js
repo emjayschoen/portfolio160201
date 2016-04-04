8668cf5491b6334d7ca8d4d47b6c7ff9b4c29998
emjayschoen <emjayschoen@gmail.com>
2016-04-03 23:15:14 -0500

    First working carousel; Grid Overlay; Added portfolio pieces.
    Carousel works on non-mobile; Grid overlay can be added at any time
    with body.test-grid; Read Dog, Ink Series, Pittore Vineyard pieces added

--- js/portfolio.js
+++ js/portfolio.js
@@ -8,8 +8,92 @@ $(document).ready(function(){
 
 
 
+	/* PIECE CAROUSEL */
+
+	// TODO: Ignore images/thumbnails that are already active
+	// TODO: Add arrows to thumbnails
+	// TODO: Add arrow keyboard control
+	// TODO: Add numeric keyboard control (treat it like chrome tabs where 1 = first image, ... , 9 = last image)
+	// TODO: Add mobile responsiveness
+	// TODO: There can only be one carousel per Piece right now
+
+
+	// Activate the first image of each carousel by default
+	/*$(".piece.--carousel .image-set").find(".image:first").load( function(){
+		activateCarouselImage($(this));
+	});*/
+
+	$(".piece.--carousel .image").click( function(event) {
+		activateCarouselImage($(this));
+	});
+
+	$(".piece.--carousel .thumbnail").click( function(event) {
+		activateCarouselThumbnail($(this));
+	});
+
+	function getCarouselActiveIndex ($piece) {
+		var $activeImage = $piece.find(".image-set .image.--active");
+		return getCarouselItemIndex($activeImage);
+	}
+	function getCarouselItemIndex ($item) {
+		return $item.index();
+	}
+
+	function goToIndex ($piece, index) {
+		var $currImageSet = $currPiece.find(".image-set");
+		var $currThumbnailSet = $currPiece.find(".thumbnail-set");
+		var $image = $currImageSet.find(".image").eq(index);
+		var $thumbnail = $currThumbnailSet.find(".thumbnail").eq(index);
+	}
+
+	function activateCarouselImage ($image) {
+		var $currPiece = $image.closest(".piece");
+		var $currImageSet = $image.closest(".image-set");
+		var $currThumbnailSet = $currPiece.find(".thumbnail-set");
+		var imageIndex = $currImageSet.find(".image").index($image);
+		var $thumbnail = $currThumbnailSet.find(".thumbnail").eq(imageIndex);
+
+		activateCarousel($currPiece, $image, $currImageSet, $thumbnail, $currThumbnailSet);
+
+	}
+
+	function activateCarouselThumbnail ($thumbnail) {
+		var $currPiece = $thumbnail.closest(".piece");
+		var $currImageSet = $currPiece.find(".image-set");
+		var $currThumbnailSet = $thumbnail.closest(".thumbnail-set");
+		var thumbnailIndex = $currThumbnailSet.find(".thumbnail").index($thumbnail);
+		var $image = $currImageSet.find(".image").eq(thumbnailIndex);
+
+		activateCarousel($currPiece, $image, $currImageSet, $thumbnail, $currThumbnailSet);
+	}
+
+	function activateCarousel ($currPiece, $image, $currImageSet, $thumbnail, $currThumbnailSet) {
+
+		// Set clicked image as only one --active
+		$currImageSet.find(".image").removeClass("--active");
+		$image.addClass("--active");
+
+		// Set corresponding thumbnail as only one --active
+		$currThumbnailSet.find(".thumbnail").removeClass("--active");
+		$thumbnail.addClass("--active");
+
+		// Move --active image
+		var leftAnchor = ($currPiece.width() - $image.width()) / 2;  /* Center-aligned */
+		/* var leftAnchor = parseInt( $("#intro").css("margin-left") );  Left-aligned */
+		var imageSetLeftOffset = $currImageSet.position().left;
+		var nextImageSetLeft = /* */ - $image.position().left + leftAnchor;
+		$currImageSet.css("transform", "translateX("+nextImageSetLeft+"px)");
+
+		console.log("leftAnchor: "+ leftAnchor);
+		console.log("nextImageSetLeft: "+ nextImageSetLeft);
+
+		// Move --active thumbnail
+		/*var nextThumbnailSetLeft = - $thumbnail.position().left + ($currPiece.width() - $thumbnail.width()) / 2;
+		$currThumbnailSet.css("left", nextThumbnailSetLeft);*/
+	}
+
 	// Piece thumbnails
-	$(".piece_.thumbnail").click( function(event){
+	/*$(".piece_.thumbnail").click( function(event){
 
 		// Get image source
 		var $imageSource = $(this).attr("src");
@@ -27,7 +111,7 @@ $(document).ready(function(){
 		// Perform transition
 
 
-	});
+	});*/
 
 
 });
